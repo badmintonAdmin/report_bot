a7300229b2492b3a88d271bf3797baa560e1baed
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-11 14:47:19 +0800

    add function get_all_data

---
+++ core/get_data.py
@@ -0,0 +1,39 @@
+from db import database
+from core.data.response import *
+from core.data.request_contract import *
+
+
+def get_all_data():
+    big_arr = []
+    sql_queries = [
+        "main.sql",
+        "lndx_pools_balance.sql",
+        "holders_counts.sql",
+        "arb_wallets_cTokens.sql",
+        "private_balance.sql",
+        "prices_cTokens.sql",
+    ]
+    db_data = {
+        query.replace(".sql", ""): database.execute_query(query)
+        for query in sql_queries
+    }
+    database.close_connection()
+
+    xTokens_data = get_xTokens_info()
+    cTokens_data = get_cTokens_info()
+    claimable = all_contract_data(xTokens_data, cTokens_data, db_data["prices_cTokens"])
+    exchange_data = exchange_balance(db_data["arb_wallets_cTokens"], claimable)
+
+    big_arr.append(all_tokens_to_usd(db_data["main"]))
+    big_arr.append(all_liquid_tokens_to_usd(db_data["main"]))
+    big_arr.append(lndx_amount(db_data["main"], db_data["lndx_pools_balance"]))
+    big_arr.append(amount_usd(db_data["main"]))
+    big_arr.append(amount_eth(db_data["main"]))
+    big_arr.append(amount_btc(db_data["main"]))
+    big_arr.append(xTokens_change(db_data["main"]))
+    big_arr.append(lndx_holders(db_data["holders_counts"]))
+    big_arr.append(xToken_holders(db_data["holders_counts"]))
+    big_arr.append(exchange_data)
+    big_arr.append(private_sold(db_data["private_balance"]))
+
+    return big_arr
--- core/run.py
+++ core/run.py
@@ -1,44 +1,5 @@
-from http.client import responses
+from core.get_data import *
 
-from db import database
-from core.data.response import *
-from core.data.request_contract import *
+data = get_all_data()
 
-
-main = database.execute_query("main.sql")
-LP_pools = database.execute_query("lndx_pools_balance.sql")
-holders = database.execute_query("holders_counts.sql")
-usd_cTokens_exchange = database.execute_query("arb_wallets_cTokens.sql")
-private_sold_amount = database.execute_query("private_balance.sql")
-
-# Get data from contract and calculate claimable
-cTokens_price = database.execute_query("prices_cTokens.sql")
-database.close_connection()
-xTokens_data = get_xTokens_info()
-cTokens_data = get_cTokens_info()
-claimable = all_contract_data(xTokens_data, cTokens_data, cTokens_price)
-
-exchange_data = exchange_balance(usd_cTokens_exchange, claimable)
-
-lndx = all_tokens_to_usd(main)
-liq = all_liquid_tokens_to_usd(main)
-lndx_change = lndx_amount(main, LP_pools)
-USD = amount_usd(main)
-eth = amount_eth(main)
-btc = amount_btc(main)
-xTokens = xTokens_change(main)
-lndx_hold = lndx_holders(holders)
-x_hold = xToken_holders(holders)
-pSold_amount = private_sold(private_sold_amount)
-
-print(lndx)
-print(liq)
-print(lndx_change)
-print(USD)
-print(eth)
-print(btc)
-print(xTokens)
-print(lndx_hold)
-print(x_hold)
-print(exchange_data)
-print(pSold_amount)
+print(data)
