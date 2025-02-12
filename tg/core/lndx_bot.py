9c19150bcc3998a470f8316b3f0c8ae2dd19fcd5
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-12 16:12:29 +0800

    add function send_message

--- tg/core/lndx_bot.py
+++ tg/core/lndx_bot.py
@@ -14,6 +14,12 @@ class LndxBot:
         self.dp.message.register(help_command, Command("help"))
         self.dp.message.register(report_command, Command("get_report"))
 
+    async def send_message(self, chat_id: str, text: str) -> None:
+        try:
+            await self.bot.send_message(chat_id=chat_id, text=text)
+        except Exception as e:
+            print(f"Error sending message: {e}")
+
     async def run(self):
         self.register_handlers()
         print("Bot running....")
