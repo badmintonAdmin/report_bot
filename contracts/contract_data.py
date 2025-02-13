d1f4460b186976052f76af732ed6fc98814c0a06
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-13 20:37:15 +0800

    refactoring contract_data

--- contracts/contract_data.py
+++ contracts/contract_data.py
@@ -1,21 +1,20 @@
 import os
 import json
 from web3 import Web3
-from dotenv import load_dotenv
-load_dotenv()
+from general_config import config
+
 
 class ContractData:
-
     def __init__(self):
-        self.url = os.getenv("URL")
-        self.sell_contract = os.getenv("SELL_CONTRACT")
+        self.url = config.URL
+        self.sell_contract = config.SELL_CONTRACT
         self.provider = Web3(Web3.HTTPProvider(self.url))
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
 
     def get_xtokens_data(self, token: str):
         try:
-            file_path = os.path.join(self.current_dir, 'abi', 'xToken.json')
-            with open(file_path, 'r') as abi_file:
+            file_path = os.path.join(self.current_dir, "abi", "xToken.json")
+            with open(file_path, "r") as abi_file:
                 contract_abi = json.load(abi_file)
         except Exception as e:
             print(f"Error loading ABI file: {e}")
@@ -28,11 +27,10 @@ class ContractData:
             print(f"Error interacting with contract: {e}")
             return None
 
-
     def get_ctokens_data(self, token: str):
         try:
-            file_path = os.path.join(self.current_dir, 'abi', 'cToken.json')
-            with open(file_path, 'r') as abi_file:
+            file_path = os.path.join(self.current_dir, "abi", "cToken.json")
+            with open(file_path, "r") as abi_file:
                 contract_abi = json.load(abi_file)
         except Exception as e:
             print(f"Error loading ABI file: {e}")
@@ -41,9 +39,10 @@ class ContractData:
             contract = self.provider.eth.contract(address=token, abi=contract_abi)
             count = contract.functions.totalSupply().call()
             balance = contract.functions.balanceOf(self.sell_contract).call()
-            return {"count": count,"balance": balance}
+            return {"count": count, "balance": balance}
         except Exception as e:
             print(f"Error interacting with contract: {e}")
             return None
+
 
 contract_data = ContractData()
