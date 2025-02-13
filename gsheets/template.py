39fc3b9308a7650f2b5aeb219b6c166271ad810b
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-13 22:57:11 +0800

    make template

--- gsheets/template.py
+++ gsheets/template.py
@@ -1,5 +1,11 @@
-from gsheets.get_data import get_epoch
+from gsheets.get_data import *
 
 
 def all_format():
-    pass
+    policies = get_policies()
+    pools = get_pools()
+    epoch = get_epoch()
+    loans = get_loans()
+    combined_list = policies + pools + epoch + loans
+    result = "\n".join(combined_list)
+    return result
