fae812cc9ade30f723cbc8c6f0c6a6c5ebcf1d76
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-13 23:06:55 +0800

    Discribe command

--- tg/core/commands_list.py
+++ tg/core/commands_list.py
@@ -1,6 +1,7 @@
 commands = {
     "/help": "Shows a list of available commands.",
     "/get_report": "Fetches the latest report available and sends it to you. If no report is available, it sends a message informing you about that.",
-    "/where_tokens": "Retrieves info on specified tokens. List tokens separated by commas without spaces (e.g., /where_tokens xBasket,USDC,USDT >1).",
     "/get_topup": "Execute the command to get information about what needs to be topped up.",
+    "Search": "=============",
+    "/where_tokens": "Retrieves info on specified tokens. List tokens separated by commas without spaces (e.g., /where_tokens xBasket,USDC,USDT >1).",
 }
