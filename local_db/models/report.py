8d3aa2a4055fee117ee1c9261db639d7a8c27ab8
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-14 22:16:16 +0800

    add model report

---
+++ local_db/models/report.py
@@ -0,0 +1,11 @@
+from sqlalchemy import DateTime, Text
+from sqlalchemy.orm import mapped_column, Mapped
+from datetime import datetime, UTC
+from .base import BaseModel
+
+
+class ReportModel(BaseModel):
+    __tablename__ = "reports"
+    date: Mapped[datetime] = mapped_column(DateTime, default=lambda: datetime.now(UTC))
+    date_report: Mapped[datetime] = mapped_column(DateTime)
+    report: Mapped[str] = mapped_column(Text)
