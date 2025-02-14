3994300aac324ec72efc9548a35a8d7d7e0145a4
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-14 10:35:36 +0800

    improve template

--- gsheets/template.py
+++ gsheets/template.py
@@ -1,11 +1,14 @@
 from gsheets.get_data import *
+from datetime import datetime
 
 
 def all_format():
+    current_date = datetime.now().strftime("*Top-Up Requirements %B %d, %Y*")
+    title = [current_date, ""]
     policies = get_policies()
     pools = get_pools()
     epoch = get_epoch()
     loans = get_loans()
-    combined_list = policies + pools + epoch + loans
+    combined_list = [*title, *policies, *pools, *epoch, *loans]
     result = "\n".join(combined_list)
     return result
