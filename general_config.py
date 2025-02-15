34293ea3ae938b09216257a544fe495b4d9c9814
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-15 22:19:32 +0800

    add requests

--- general_config.py
+++ general_config.py
@@ -1,6 +1,4 @@
 from dotenv import dotenv_values
-import os
-from pathlib import Path
 
 
 class Config:
@@ -8,9 +6,6 @@ class Config:
         values = dotenv_values()
         for key, value in values.items():
             setattr(self, key, value)
-        BASE_DIR = Path(__file__).resolve().parent.parent
-        DB_PATH = BASE_DIR / self.DATABASE_URL
-        self.DATABASE_URL = f"sqlite+aiosqlite:///{DB_PATH}"
 
     def __repr__(self):
         return f"{self.__dict__}"
