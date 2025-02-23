6bdd4468295b623dd391a5a9dab3a0d5bee8d2b1
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-23 11:22:22 +0800

    change parse mode

--- tg/core/base_bot.py
+++ tg/core/base_bot.py
@@ -4,5 +4,5 @@ from aiogram.client.default import DefaultBotProperties
 
 class BaseBot(Bot):
     def __init__(self, bot_token):
-        default_properties = DefaultBotProperties(parse_mode="Markdown")
+        default_properties = DefaultBotProperties(parse_mode="HTML")
         super().__init__(token=bot_token, default=default_properties)
