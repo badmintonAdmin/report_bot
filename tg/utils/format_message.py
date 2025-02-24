2ad27a447dd75f04b8d21e7919c5924f5a4c4bfc
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-24 11:05:13 +0800

    change search to HTML

--- tg/utils/format_message.py
+++ tg/utils/format_message.py
@@ -1,8 +1,11 @@
+from aiogram.utils import markdown as m
+
+
 def format_where_tokens(data):
-    content = ["*===Tokens that were found===*"]
+    content = [m.hbold("===Tokens that were found===")]
     for index, row in data.iterrows():
         content.append(
-            f'{index} -Token: {row["token"]} | Address: *{row["address"]}* | Chain: {row["chain"]} | Amount: *{row["amount"]:,.2f}*'
+            f'{index} -Token: {row["token"]} | Address: {m.hbold(row["address"])} | Chain: {row["chain"]} | Amount: {m.hbold(f'{row["amount"]:,.2f}')}'
         )
         content.append("=" * 32)
 
