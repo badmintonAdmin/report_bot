0c3e01d3102fb9c325e0f8ed8e4caee842f4772a
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-25 17:09:05 +0800

    add command price

--- tg/core/commands_list.py
+++ tg/core/commands_list.py
@@ -4,4 +4,5 @@ commands = {
     "/get_topup": "Execute the command to get information about what needs to be topped up.",
     "Search": "=============",
     "/where_tokens": "Retrieves info on specified tokens. List tokens separated by commas without spaces (e.g., /where_tokens xBasket,USDC,USDT >1).",
+    "/price": "Retrieves current prices for specified tokens. List tokens separated by commas without spaces (e.g., /price LNDX,eth).",
 }
